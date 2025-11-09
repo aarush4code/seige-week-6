1073e1c0ab7ceaa1f8722f21695f1fa0e8694ea0
Aarush <aarushchougule4@gmail.com>
2025-11-08 18:53:32 -0600

    initiial commit

---
+++ cars.py
@@ -0,0 +1,27 @@
+class  Car:
+    
+
+
+    def __init__(self,make,model,year,color,price):
+        self.make = make 
+        self.model = model
+        self.year = year
+        self.color = color
+        self.price = price
+
+
+    def drive(self):
+        print("The car is driving!")
+
+    def stop(self):
+        print("The car is stopped!")
+
+
+"""
+    make =  str("Honda")
+    model = str("Civic")
+    year = int(2023)
+    color = str("Black")
+    price = int(22,000)
+"""
+    
---
+++ showroom.py
@@ -0,0 +1,28 @@
+class Showroom:
+
+    def __innit__(self,location,year_start, total_sales, employees):
+        self.location =  None
+        self.year_start = None
+        self.total_sales = None
+        self.employees = None
+
+
+    def inventory(self):
+        print("You just bought a car!")
+
+    def sales(self):
+        print("You just sold a car!")
+
+
+
+
+
+
+
+
+"""
+    location =  str("LA")
+    year established = int(1993)
+    total cars sold = int(312)
+    employees = int(24)
+"""
